92e07970abf60cf846242c98ca790e3ee99cb5e4
Rohitphalke2406 <81799042+Rohitphalke2406@users.noreply.github.com>
2024-01-12 20:15:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+var divElement = document.getElementById("div1");
+
+divElement.style.color = "blue";            
+divElement.style.backgroundColor = "yellow";
+divElement.style.margin = "20px";           
+divElement.style.padding = "10px";          
+divElement.style.fontSize = "18px";         
+divElement.style.fontWeight = "bold";       
+divElement.style.height = "200px";          
+divElement.style.width = "300px";           
